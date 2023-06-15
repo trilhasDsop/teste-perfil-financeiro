0d252fbe26af7680f43b483583b4831f26b03952
trilhasDsop <gabriel.laminas@dsop.com.br>
2023-06-15 13:05:54 -0300

    Resultados desktop - em desenvolvimento
    Inserção de imagem das capas da playlist

--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -2,6 +2,69 @@ import React from 'react';
 import ilustracao from '../../assets/img/Ilustracao2.svg';
 import styleHome from '../../styles/page/home.module.scss';
 
+const questoes = [
+   {
+      'questao': 'O que você ganha por mês é suficiente para arcar com os seus gastos?',
+      'alternativas': [
+         {
+            'letra': 'A',
+            'texto': 'É suficiente, mas não sobra nada.',
+            'pontos': 5 
+         },
+         {
+            'letra': 'B',
+            'texto': 'Consigo pagar minhas contas e ainda guardo mais 10% dos meus ganhos todo mês.',
+            'pontos': 10 
+         },
+         {
+            'letra': 'C',
+            'texto': 'Gasto todo o meu dinheiro e ainda uso o limite de cheque especial ou peço emprestado para parentes e amigos.',
+            'pontos': 0 
+         }
+      ]
+   },
+   {
+      'questao': 'Você tem conseguido pagar suas despesas em dia e à vista?',
+      'alternativas': [
+         {
+            'letra': 'A',
+            'texto': 'Sempre parcelo os meus compromissos e utilizo linhas de crédito como cheque especial, cartão de crédito e crediário.',
+            'pontos': 0 
+         },
+         {
+            'letra': 'B',
+            'texto': 'Pago em dia, à vista e, em alguns casos, com bons descontos.',
+            'pontos': 10 
+         },
+         {
+            'letra': 'C',
+            'texto': 'Quase sempre, mas tenho que parcelar as compras de maior valor.',
+            'pontos': 5 
+         }
+      ]
+   },
+   {
+      'questao': 'Você realiza seu orçamento financeiro mensalmente?',
+      'alternativas': [
+         {
+            'letra': 'A',
+            'texto': 'Somente registro o realizado, sem analisar os gastos.',
+            'pontos': 5 
+         },
+         {
+            'letra': 'B',
+            'texto': 'Não faço o meu orçamento financeiro.',
+            'pontos': 0 
+         },
+         {
+            'letra': 'C',
+            'texto': 'Faço periodicamente e comparo o orçado com o realizado.',
+            'pontos': 10 
+         }
+      ]
+   },
+]
+
 export const Home = () => {
    return (
       <>
@@ -20,61 +83,78 @@ export const Home = () => {
 
          <section className={styleHome.section1__home}>
             <div className={styleHome.section1__homecontainer}>
-            <h1>Inicie seu questionário
-               <svg width="18" height="5" viewBox="0 0 18 5" fill="none" xmlns="http: www.w3.org/2000/svg">
-                  <path d="M1 1L9 3L17 1" stroke="#12FF00" stroke-width="2" stroke-linecap="round"/>
-               </svg>
-            </h1>
+               <h1>Inicie seu questionário
+                  <svg width="18" height="5" viewBox="0 0 18 5" fill="none" xmlns="http: www.w3.org/2000/svg">
+                     <path d="M1 1L9 3L17 1" stroke="#12FF00" strokeWidth="2" strokeLinecap="round"/>
+                  </svg>
+               </h1>
 
-            <article className={styleHome.section1__home__article}>
-               <h2>O que você ganha por mês é suficiente para arcar com os seus gastos?</h2>
-               <ul className={styleHome.section1__home__articleQuestoes}>
-                  <li>
-                  <span>A</span> Consigo pagar meus gastos e ainda guardo parte dos meus ganhos.
-                  </li>
-                  <li>
-                  <span>B</span> É suficiente, mas não sobra nada.
-                  </li>
-                  <li>
-                  <span>C</span> Gasto todo o meu dinheiro e ainda utilizo linhas de crédito para cumprir com meus compromissos do mês.
-                  </li>
-               </ul>
-            </article>
+               <article className={styleHome.section1__home__article}>
+                  <h2>O que você ganha por mês é suficiente para arcar com os seus gastos?</h2>
+                  <ul className={styleHome.section1__home__articleQuestoes}>
+                     <li>
+                     <span>A</span> Consigo pagar meus gastos e ainda guardo parte dos meus ganhos.
+                     </li>
+                     <li>
+                     <span>B</span> É suficiente, mas não sobra nada.
+                     </li>
+                     <li>
+                     <span>C</span> Gasto todo o meu dinheiro e ainda utilizo linhas de crédito para cumprir com meus compromissos do mês.
+                     </li>
+                  </ul>
+               </article>
 
-            <article className={styleHome.section1__home__article}>
-               <h2>O seu seguro de vida é suficiente para garantir sua independência financeira e/ou invalidez?</h2>
-               <ul className={styleHome.section1__home__articleQuestoes}>
-                  <li>
-                  <span>A</span> Consigo pagar meus gastos e ainda guardo parte dos meus ganhos.
-                  </li>
-                  <li>
-                  <span>B</span> É suficiente, mas não sobra nada.
-                  </li>
-                  <li>
-                  <span>C</span> Gasto todo o meu dinheiro e ainda utilizo linhas de crédito para cumprir com meus compromissos do mês.
-                  </li>
-               </ul>
-            </article>
+               <article className={styleHome.section1__home__article}>
+                  <h2>O seu seguro de vida é suficiente para garantir sua independência financeira e/ou invalidez?</h2>
+                  <ul className={styleHome.section1__home__articleQuestoes}>
+                     <li>
+                     <span>A</span> Consigo pagar meus gastos e ainda guardo parte dos meus ganhos.
+                     </li>
+                     <li>
+                     <span>B</span> É suficiente, mas não sobra nada.
+                     </li>
+                     <li>
+                     <span>C</span> Gasto todo o meu dinheiro e ainda utilizo linhas de crédito para cumprir com meus compromissos do mês.
+                     </li>
+                  </ul>
+               </article>
 
-            <article className={styleHome.section1__home__article}>
-               <h2>Você realiza seu orçamento financeiro?</h2>
-               <ul className={styleHome.section1__home__articleQuestoes}>
-                  <li>
-                  <span>A</span> Não faço meu orçamento financeiro.
-                  </li>
-                  <li>
-                  <span>B</span> Somente registro o realizado mas não faço qualquer análise.
-                  </li>
-                  <li>
-                  <span>C</span> Faço periodicamente, analiso e comparo o orçado com o realizado.
-                  </li>
-               </ul>
-            </article>
+               <article className={styleHome.section1__home__article}>
+                  <h2>Você realiza seu orçamento financeiro?</h2>
+                  <ul className={styleHome.section1__home__articleQuestoes}>
+                     <li>
+                     <span>A</span> Não faço meu orçamento financeiro.
+                     </li>
+                     <li>
+                     <span>B</span> Somente registro o realizado mas não faço qualquer análise.
+                     </li>
+                     <li>
+                     <span>C</span> Faço periodicamente, analiso e comparo o orçado com o realizado.
+                     </li>
+                  </ul>
+               </article>
 
-            <div className={styleHome.section1__home__divbutton}>
-               <a href="#">Resetar</a>
-               <a href="#">Enviar</a>
-            </div>
+               {/* {
+                  questoes && questoes.map((q, index) => (
+                     <article key={index} className={styleHome.section1__home__article}>
+                        <h2>{q.questao}</h2>
+                        {q.alternativas.map((alternativa, i) => (
+                           <ul key={i} className={styleHome.section1__home__articleQuestoes}>
+                              <li><span>{alternativa.letra}</span>
+                                 {alternativa.texto}
+                              </li>
+                           </ul>
+                        ))}
+                        
+                     </article>
+                  ))
+               } */}
+
+
+               <div className={styleHome.section1__home__divbutton}>
+                  <a href="#">Resetar</a>
+                  <a href="resultado">Enviar</a>
+               </div>
             </div>
          </section>
 
--- src/Page/Resultado/Resultado.jsx
+++ src/Page/Resultado/Resultado.jsx
@@ -1,7 +1,96 @@
-import React from 'react'
+import React from 'react';
+import styleHome from '../../styles/page/home.module.scss';
+import styleResultado from '../../styles/page/resultado.module.scss';
+
+import img1 from '../../assets/capas/Equilibrado/Comportamento Do Ser Rico.png';
+import img2 from '../../assets/capas/Equilibrado/Nome sujo pode ser a solução.png';
+import img3 from '../../assets/capas/Equilibrado/Dinheiro Na Crise.png';
 
 export const Resultado = () => {
   return (
-    <div>Página de Resultado</div>
+    <section className={styleResultado.sectionResultado__container}>
+      <article className={styleResultado.sectionResultado__articlePerfil}>
+        <div className={styleResultado.sectionResultado__articlePerfil__divflex}>
+          <h1><span>Perfil</span><br/>Equilibrado</h1>
+          <p>
+            A sua situação financeira de equilibrado financeiramente para endividado está há poucos passos de distância. É preciso atitudes imediatas de combater a causa dos problemas já apresentados neste teste. O envolvimento familiar, caso tenha família, é imprescindível. As mudanças de hábitos e comportamentos em relação ao dinheiro também é necessária. Busque pelos sonhos e propósitos, eles que serviram como agentes motivadores.<br/>
+            <strong>Ph.D Reinaldo Domingos</strong>
+          </p>
+        </div>
+      </article>
+
+      <article className={styleResultado.sectionResultado__article2Container}>
+        <h1>Seus resultados
+          <svg width="18" height="5" viewBox="0 0 18 5" fill="none" xmlns="http: www.w3.org/2000/svg">
+              <path d="M1 1L9 3L17 1" stroke="#12FF00" strokeWidth="2" strokeLinecap="round"/>
+          </svg>
+        </h1>
+
+        <article className={styleHome.section1__home__article}>
+          <h2>O que você ganha por mês é suficiente para arcar com os seus gastos?</h2>
+          <ul className={styleHome.section1__home__articleQuestoes}>
+            <li>
+              <span>A</span> Consigo pagar meus gastos e ainda guardo parte dos meus ganhos.
+            </li>
+          </ul>
+          <p>
+            Este é um dos requisitos imprescindível para uma pessoa destinar seus recursos ganhos e gastos, de forma estruturada para que possa buscar os propósitos, sonhos e necessidades
+          </p>
+        </article>
+
+        <article className={styleHome.section1__home__article}>
+          <h2>O seu seguro de vida é suficiente para garantir sua independência financeira e/ou invalidez?</h2>
+          <ul className={styleHome.section1__home__articleQuestoes}>
+            <li>
+              <span>B</span> É suficiente, mas não sobra nada.
+            </li>
+          </ul>
+          <p>
+            Sua atitude mostra que você está no meio do caminho, ou seja, tem e/ou teve seguro, mas o mesmo não garante a sustentabilidade financeira em caso de um sinistro.
+          </p>
+        </article>
+
+        <article className={styleHome.section1__home__article}>
+          <h2>Você realiza seu orçamento financeiro?</h2>
+          <ul className={styleHome.section1__home__articleQuestoes}>
+            <li>
+              <span>B</span> Somente registro o realizado mas não faço qualquer análise.
+            </li>
+          </ul>
+          <p>
+            Anotar somente sem analisar, não remeterá a atitudes necessárias para tomada de decisões em relação a movimentação financeira, preventivas e/ou corretivas.
+          </p>
+        </article>
+      </article>
+
+      <article className={styleResultado.sectionResultado__article3Container}>
+        <h1>Seus resultados: Equilibrado
+          <svg width="18" height="5" viewBox="0 0 18 5" fill="none" xmlns="http: www.w3.org/2000/svg">
+              <path d="M1 1L9 3L17 1" stroke="#12FF00" strokeWidth="2" strokeLinecap="round"/>
+          </svg>
+        </h1>
+
+        <div className={styleResultado.sectionResultado__gridlista}>
+          <div className={styleResultado.sectionResultado__gridlista__container}>
+            <img src={img1} alt="" />
+            <a href="">Comportamento do ser rico</a>
+          </div>
+
+          <div className={styleResultado.sectionResultado__gridlista__container}>
+            <img src={img2} alt="" />
+            <a href="">Nome sujo pode ser a solução</a>
+          </div>
+
+          <div className={styleResultado.sectionResultado__gridlista__container}>
+            <img src={img3} alt="" />
+            <a href="">Dinheiro Na Crise</a>
+          </div>
+        </div>
+
+        <div className={styleResultado.sectionResultado__divplaylist}>
+          <a href="playlist">Ver mais</a>
+        </div>
+      </article>
+    </section>
   )
 }
