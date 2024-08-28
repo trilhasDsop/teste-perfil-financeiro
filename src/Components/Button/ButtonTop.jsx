f60882c7309da8bc491443e592005b637b2dda22
trilhasDsop <109526716+trilhasDsop@users.noreply.github.com>
2024-08-28 11:17:28 -0300

    Remoção de botão para o topo mobile

--- src/Components/Button/ButtonTop.jsx
+++ src/Components/Button/ButtonTop.jsx
@@ -32,8 +32,8 @@ const ButtonToTop = () => {
          onClick={handleClick}
       >
          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 19V5" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-            <path d="M5 12L12 5L19 12" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            <path d="M12 19V5" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M5 12L12 5L19 12" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
          </svg>
      </button>
    )
