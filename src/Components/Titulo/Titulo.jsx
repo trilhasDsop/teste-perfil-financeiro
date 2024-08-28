6f8299c680032a63daee643976f94009dd58fb2c
trilhasDsop <109526716+trilhasDsop@users.noreply.github.com>
2024-08-28 16:28:28 -0300

    Inserindo movimento e icone novo no titulo

--- src/Components/Titulo/Titulo.jsx
+++ src/Components/Titulo/Titulo.jsx
@@ -4,8 +4,8 @@ import tituloSection from '../../styles/components/titulo.module.scss'
 const Titulo = React.memo(function Titulo({children}){
    return (
     <h1 className={tituloSection.titulosection}>{children}
-      <svg width="18" height="5" viewBox="0 0 18 5" fill="none" xmlns="http: www.w3.org/2000/svg">
-         <path d="M1 1L9 3L17 1" stroke="#12FF00" strokeWidth="2" strokeLinecap="round"/>
+      <svg width="18" height="7" viewBox="0 0 18 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <path d="M1 1L9 5L17 1" stroke="#12FF00" strokeWidth="2" strokeLinecap="round"/>
       </svg>
     </h1>
    )
